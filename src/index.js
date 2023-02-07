f39ac1e127607e50c473b2f42d5e3f47d574cc3c
kehinde cwb <85681159+codewithbillions@users.noreply.github.com>
2023-02-07 18:06:38 +0100

    updated directory page with directory component and category

--- src/index.js
+++ src/index.js
@@ -15,3 +15,17 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// import React from 'react';
+// import {createRoot} from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const container = document.getElementById('root')
+// const root = createRoot(container)
+// root.render(
+//   <React.StrictMode>
+//      <App />
+//    </React.StrictMode>
+// )
